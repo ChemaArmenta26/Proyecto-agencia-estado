3e6d846dc11ba7c5af1ba39597a8447d8754d7e3
ChemaArmenta26 <159948311+ChemaArmenta26@users.noreply.github.com>
2024-03-20 14:24:38 -0700

    Capas Negocio y Presentacion

---
+++ ProyectoAgenciaNegocio/src/main/java/com/mycompany/proyectoagencianegocio/ProyectoAgenciaNegocio.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.proyectoagencianegocio;
+
+/**
+ *
+ * @author PC
+ */
+public class ProyectoAgenciaNegocio {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
